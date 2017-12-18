ac9f7ab413dc2a955613cedd8e5b0db1701b087f
zemla4ok <dima-vital@live.com>
2017-12-18 22:47:16 +0300

    nearest city to minsk

--- co.js
+++ co.js
@@ -18,5 +18,26 @@ async function start(){
 
     console.log();
     console.log('task 2');
+    await co(function* () {
+        return yield Promise.mapSeries(
+            [
+            `https://maps.googleapis.com/maps/api/geocode/json?address=Minsk`,
+            `https://maps.googleapis.com/maps/api/geocode/json?address=Copenhagen`,
+            `https://maps.googleapis.com/maps/api/geocode/json?address=Oslo`,
+            `https://maps.googleapis.com/maps/api/geocode/json?address=Brussel`
+        ], async function (item) {
+            return await axios.get(item);
+        });
+    }).then(function (value) {
+        let cities = {};
+        value.map((city) => {
+            console.log(city.data.results[0].formatted_address);
+            cities[city.data.results[0].formatted_address] = city.data.results[0].geometry.location;
+        });
+        console.log(geolib.findNearest(cities['Minsk, Belarus'], cities, 1));
+    });
+
+    console.log();
+    console.log('task 3');
 
 }
